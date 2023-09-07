fe5f5d408e21fa0a25e20f7b20d8ceb6dac5adaa
Daniel Temkin <d@danieltemkin.com>
2023-09-06 22:35:25 -0400

    loads recipes from files

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# BAKEHOUSE WORKSHOP TEMPLATE
+# MERITA
 =================
+
+A bread-based esolang for Bakehouse http://www.bacfl.org/
 
 This uses two technologies that can be used directly from their sites:
 * [PEG.js](https://pegjs.org/online)
 * [Shader Park](https://shaderpark.netlify.com/).
+
+Inspirations:
+* [Chef](https://www.dangermouse.net/esoteric/chef.html) by David Morgan-Mar 
+    * [interview with David](https://esoteric.codes/blog/david-morgan-mar) for esoteric.codes
+* [in:verse](https://inverse.website/) by Sukanya Aneja
+    * [esoteric.codes piece on in:verse](https://esoteric.codes/blog/inverse)
--- main.js
+++ main.js
@@ -1,27 +1,69 @@
 const codeblock = document.getElementById("script");
-const pegcodeblock = document.getElementById("source");
 const draw = document.getElementById("draw");
+
+const recipes = [
+  "CANAPES",
+  "DEVILED TONGUE",
+  "PINWHEELS",
+  "CORNED BEEF HASH",
+  "HOT CHEESE-TOMATO",
+  "RIBBONS",
+  "CRAB CANAPES",
+  "MINCED CHICKEN",
+  "ROLLED",
+  "CREAMED SHRIMP ON TOAST",
+  "ORANGE ANGEL FOOD TORTE"
+];
+
+const default_recipe = "CREAMED SHRIMP ON TOAST";
+
+const update_dropdown = () => {
+  //    alert(document.getElementById("recipe").value);
+  var client = new XMLHttpRequest();
+  client.open('GET', `./recipes/${document.getElementById("recipe").value}`);
+  client.onreadystatechange = function() {
+    // alert(client.responseText);
+    document.getElementById("source").innerHTML = client.responseText.replace("\n","<br>");
+  }
+  client.send();
+
+}
 
 window.addEventListener('load', function() { 
   // if the function is loaded yet
   if (typeof sculptToMinimalRenderer === "function") 
     sculptToMinimalRenderer(document.querySelector('#output_canvas'), codeblock.value);
 
+  const recipe_list = document.getElementById("recipe");
+  for(let i = 0; i < recipes.length; i++) {
+    var opt = document.createElement('option');
+    opt.value = recipes[i] + ".recipe";
+    opt.innerHTML = recipes[i];
+    if (recipes[i] == default_recipe)
+      opt.selected = true;
+    recipe_list.appendChild(opt);
+  }
+
+  recipe_list.addEventListener("change", update_dropdown);
+  update_dropdown();
+
   document.getElementById("updateBtn").addEventListener("click", function() {
     let errorOutput = document.getElementById("error");
     errorOutput.innerHTML = "";
     let newCanv = document.createElement("canvas");
-    try {
-      sculptToMinimalRenderer(newCanv, codeblock.value);
-    }
-    catch(e) {
-      errorOutput.innerText = e;
-    }
+    // try {
+      // sculptToMinimalRenderer(newCanv, codeblock.value);
+    // }
+    // catch(e) {
+    //   errorOutput.innerText = e;
+    // }
+    let draw = document.getElementById("draw");
     draw.innerHTML = null;
     draw.appendChild(newCanv);
   });
   
   document.getElementById("pegUpdateBtn").addEventListener("click", function() {
+    const pegcodeblock = document.getElementById("source");
     recipe_nodes = recipeParser.parse(pegcodeblock.value);
     alert(
       `TITLE: ${recipe_nodes.title}\n` +
